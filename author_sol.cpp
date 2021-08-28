794d3522f8e1c14ea18af6fa438af3ba05aba2f4
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-28 16:39:16 +0800

    Update author_sol.cpp

--- author_sol.cpp
+++ author_sol.cpp
@@ -14,18 +14,18 @@ int main() {
 		for (int i = 0; i < n; i++) {
 			int x;
 			cin >> x;
-			// Count the frequency of each element.
+			// count the frequency of each element
 			m[x]++;
 		}
 		int ans = n;
 		for (auto [element, frequency] : m) {
-			// Find the minimum value of the difference between 'n' and every frequency of an element.
+			// find the minimum value of the difference between 'n' and every frequency of an element
 			ans = min(ans, n - frequency);
 		}
-		// If 'n' is less than or equal to 2, then there is nothing to do (0-deletion).
-		// Otherwise, choose the the minimum value between 'ans' and ('n' - 2).
-		// The ('n' - 2) case is wherein all the numbers are unique from each other
-		// so we need to keep 2 elements and remove the remaining ('n' - 2) elements.
+		// if 'n' is less than or equal to 2, then there is nothing to do (0-deletion)
+		// otherwise, choose the the minimum value between 'ans' and ('n' - 2)
+		// the ('n' - 2) case is wherein all the numbers are unique from each other
+		// so we need to keep 2 elements and remove the remaining ('n' - 2) elements
 		cout << (n <= 2 ? 0 : min(ans, n - 2)) << '\n';
 	}
 	return 0;
